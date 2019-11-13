614f3aee4552fbbb195801b739a284c561b4e2f3
Ruoshui <rmao10@stuy.edu>
2019-11-13 15:39:04 -0500

    feat: done

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <sys/stat.h>
+#include <time.h>
+#define F "./main"
+
+int main(void)
+{
+    struct stat md;
+    puts("File: "F);
+    stat(F, &md);
+    printf("size: %ld\n", md.st_size);
+    printf("mode: %d\n", md.st_mode);
+    printf("last accessed: %s\n", ctime(&md.st_atime));
+
+    return 0;
+}
